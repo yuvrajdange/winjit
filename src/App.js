c0407612e9056a02efdcdc1392eb9dce771cc0f7
yuvrajdange <98180552+yuvrajdange@users.noreply.github.com>
2022-05-05 16:10:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-// import UseHook from './components/UseHook';
-// import OwnHook from './components/OwnHook';
 import Parent from "./components/Parent";
 import CallParent from "./components/CallParent";
 function App() {
   return (
     <div className="App">
-      {/* <h2>Hooks</h2>
-      <UseHook />
-      <OwnHook /> */}
+      
       <Parent />
       <CallParent />
     </div>
